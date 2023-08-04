892b6e86e3a917c55ec1ef47e39c27194d724739
hayato94087 <hayato94087@gmail.com>
2023-08-05 05:17:32 +0900

    ユーザーボタンを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className="text-lg">
-      テストページ
-    </main>
-  )
+    <>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 20,
+        }}
+      >
+        <h1>My App</h1>
+        <SignedIn>
+          {/* Mount the UserButton component */}
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+        <SignedOut>
+          {/* Signed out users get sign in button */}
+          <SignInButton />
+        </SignedOut>
+      </header>
+      <main className="text-lg">テストページ</main>
+    </>
+  );
 }
